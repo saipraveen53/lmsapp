a7cca1006613a7648cd0e1f767afd9386aca92b7
saipraveen53 <saipraveenthandra99@gmail.com>
2025-12-18 21:46:16 +0530

    File:

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// saipraveen53/moviesapp/MoviesApp-dd9a6136fae392be99abe4dfb5ccd9e382e1d973/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}", 
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
