b8efe095ab97b9f647c416eff867d7814c9f531b
Mahanth17 <bsp11772@example.com>
2025-12-19 15:30:35 +0530

    updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout() {
           <Stack.Screen name="index" options={{headerShown:false}}  />
           <Stack.Screen name="(auth)" options={{headerShown:false}} />
           <Stack.Screen name="(dashboard)" options={{headerShown:false}} />
+          <Stack.Screen name="(admin)" options={{headerShown:false}} />
         </Stack>
     </LmsContext>
   );
