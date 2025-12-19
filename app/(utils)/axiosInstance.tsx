f4939f29c99437fa350092e9161195f897e4d404
saipraveen53 <145278161+saipraveen53@users.noreply.github.com>
2025-12-19 15:23:35 +0530

    Updated :

---
+++ app/(utils)/axiosInstance.tsx
@@ -0,0 +1,43 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios, {
+    AxiosError,
+    AxiosInstance,
+    InternalAxiosRequestConfig,
+} from 'axios';
+ 
+
+
+const getToken = async (): Promise<string | null> => {
+  try {
+    return await AsyncStorage.getItem("accessToken");
+  } catch (error) {
+    console.error("Error getting token:", error);
+    return null;
+  }
+};
+
+const createAxiosInstance = (baseURL: string): AxiosInstance => {
+  const instance: AxiosInstance = axios.create({
+    baseURL: baseURL,
+    timeout: 10000,  
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  instance.interceptors.request.use(
+    async (config: InternalAxiosRequestConfig): Promise<InternalAxiosRequestConfig> => {
+      const token: string | null = await getToken();
+      
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error: AxiosError): Promise<AxiosError> => Promise.reject(error)
+  );
+
+  return instance;
+};
+
+export const rootApi: AxiosInstance = createAxiosInstance("http://192.168.0.216:8080");
